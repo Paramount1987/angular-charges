8d288859d7d3d7c1a15a7d5fad81dd447fe75b2d
Paramount2009 <Paramount7891@gmail.com>
2016-02-17 16:42:09 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 var arrChart = [];
+var costsLength = 0;
+var chargeslength = 0;
 
 var app = angular.module('ChargeApp',[]);	
 
 app.controller('ChargeAppCtrl', function($scope,$filter){
-	$scope.charges = [{label: 'доход 1', value: 60, color: "#008000", status: true}, {label: 'доход 2', value: 40, color: "#008000", status: true}, {label: 'доход 3', value: 30, color: "#008000",status: true},{label: 'доход 4', value: 100, color: "#008000",status: true}];
-	$scope.costs = [{label: 'расход 1', value: 160, color: "#ff0000", status: true}];
+	$scope.charges = [{label: 'доход 1', value: 60, color: "#008000", status: true, index: 0}, {label: 'доход 2', value: 30, color: "#008000",status: true, index: 1},{label: 'доход 3', value: 100, color: "#008000",status: true, index: 2}];
+	$scope.costs = [{label: 'расход 1', value: 160, color: "#ff0000", status: true,index:3}];
+
+	chargeslength = $scope.charges.length;
+	costslength = $scope.costs.length;
+
 	$scope.reverseByMoneyCharge = false;
 	$scope.reverseByMoneyCost = false;
 	
 
 	$scope.createChart = function(){
 
-		arrChart = $scope.costs.concat($scope.charges);
+		arrChart = $scope.charges.concat($scope.costs);
 		var ctx = document.getElementById("chart-area").getContext("2d");	
 		window.PieChart = new Chart(ctx).Pie(arrChart, {
 				responsive:true
@@ -36,47 +42,40 @@ app.controller('ChargeAppCtrl', function($scope,$filter){
 		}
 	};
 
-	$scope.getTotalSum = function(index){		
+	$scope.getTotalSum = function(index,indexSort){		
 		$scope.totalSum = 0;
 		$scope.compoteTotalSum();
 
-		PieChart.segments[index + $scope.costs.length].fillColor = $scope.charges[index].status? "#008000" : "#8AC18A";
+		if(index === undefined || indexSort === undefined) return;
+
+		PieChart.segments[$filter('searchChartId')(PieChart.segments, index)].fillColor = $scope.charges[indexSort].status? "#008000" : "#8AC18A";
 		PieChart.update();
+
 	};
-	$scope.getTotalCost = function(index){
+	$scope.getTotalCost = function(index,indexSort){
+
 		$scope.totalCost = 0;
 		$scope.compoteTotalCost();
 
-		PieChart.segments[index].fillColor = $scope.costs[index].status? "#ff0000" : "#FFBDBD";
+		if(index === undefined || indexSort === undefined) return;
+
+		PieChart.segments[$filter('searchChartId')(PieChart.segments, index)].fillColor = $scope.costs[indexSort].status? "#ff0000" : "#FFBDBD";
 		PieChart.update();
 		
 	};
-	$scope.removeCharge = function(index){
-		PieChart.removeData(index + $scope.costs.length);
-		$scope.charges.splice(index,1);
+	$scope.removeCharge = function(index,indexSort){
+		PieChart.removeData($filter('searchChartId')(PieChart.segments, index));
+		$scope.charges.splice(indexSort,1);
+		chargeslength = $scope.charges.length;
 		$scope.getTotalSum();
 	};
-	$scope.removeCost = function(index){
-		PieChart.removeData(index);
-		$scope.costs.splice(index,1);
+	$scope.removeCost = function(index,indexSort){
+		PieChart.removeData($filter('searchChartId')(PieChart.segments, index));
+		$scope.costs.splice(indexSort,1);
+		costslength = $scope.costs.length;
 		$scope.getTotalCost();
 	};
-	$scope.valueEdit = function(event,index,obj){
 
-		var input = document.createElement('input');
-		input.setAttribute('type','number');
-		input.setAttribute('class','td-input-item');
-		input.setAttribute('data-index',index);
-		input.value = obj.value;
-		event.currentTarget.appendChild(input);
-		input.focus();
-
-	input.onblur = function(){
-			$scope.charges[this.getAttribute('data-index')].value = this.value;
-			this.parentNode.removeChild(this);
-		};
-
-	};
 	/////////////////////////////////////////////sort
 	$scope.sortByMoney = function(value){
 
@@ -97,26 +96,110 @@ app.controller('ChargeAppCtrl', function($scope,$filter){
 });
 
 /////////////////////////////////////////////////////Add charge
-app.controller('AddChargeCtrl', function($scope){
+app.controller('RemoteChargeCtrl', function($scope,$filter){
 
 	$scope.addCharge = function(){
-		$scope.charges.push({label: $scope.labelCharge, value: $scope.valueCharge, status: true,color: "#008000"});
+		$scope.charges.push({label: $scope.labelCharge, value: $scope.valueCharge, status: true,color: "#008000",index: chargeslength + costslength});
+		chargeslength = $scope.charges.length;
 		PieChart.addData({label: $scope.labelCharge, value: $scope.valueCharge, status: true,color: "#008000"});
 		$scope.labelCharge = "";
 		$scope.valueCharge = "";
 		$scope.getTotalSum();//total sum
 	};
+
+	$scope.removeAllCharges = function(){
+		PieChart.destroy();
+		var ctx = document.getElementById("chart-area").getContext("2d");	
+		PieChart = new Chart(ctx).Pie($scope.costs, {
+				responsive:true
+		});
+		
+		chargeslength = $scope.charges.length = 0;
+		$scope.getTotalSum();//total sum
+	};
 });
 //////////////////////////////////////////////////////Add Cost
-app.controller('AddCostCtrl', function($scope){
+app.controller('RemoteCostCtrl', function($scope,$filter){
 
 	$scope.addCost = function(){
-		$scope.costs.push({label: $scope.labelCost, value: $scope.valueCost, status: true,color: "#ff0000"});
+		$scope.costs.push({label: $scope.labelCost, value: $scope.valueCost, status: true,color: "#ff0000",index: chargeslength + costslength});
+		costslength = $scope.costs.length;
 		PieChart.addData({label: $scope.labelCost, value: $scope.valueCost, status: true,color: "#ff0000"});
 		$scope.labelCost = "";
 		$scope.valueCost = "";
 		$scope.getTotalCost();//total sum
 	};
-});
-////////////////////////////////////////////
 
+	$scope.removeAllCosts = function(){
+
+		PieChart.destroy();
+		var ctx = document.getElementById("chart-area").getContext("2d");	
+		PieChart = new Chart(ctx).Pie($scope.charges, {
+				responsive:true
+		});
+		
+		costslength = $scope.costs.length = 0;
+		$scope.getTotalCost();//total sum
+	};
+});
+////////////////////////////////////////////directive
+app.directive('editInput',function($filter){
+	return {
+		restrict: 'E',
+		replace: true,
+		template: function(elem,attr){
+			var typeValue = attr.type + ".value";
+			var typeIndex = attr.type + ".index"
+			return	'<input type="number" class="td-input-item transparent" ng-model="' + typeValue + '" value="{{' + typeValue + '}}" index={{' + typeIndex + '}} />';
+		},
+		link: function(scope,element,attrs){
+				element.on('change',function(event){
+					 PieChart.segments[$filter('searchChartId')(PieChart.segments, +attrs.index)].value = attrs.value;
+					 PieChart.update();
+				});
+				element.on('blur',function(){
+					element.addClass('transparent');
+				});
+				element.on('focus',function(){
+					element.removeClass("transparent");
+				});
+		}
+	};
+});
+
+app.directive('editTitle',function($filter){
+	return {
+		restrict: 'E',
+		replace: true,
+		template: function(elem,attr){
+			var typeValue = attr.type + ".label";
+			var typeIndex = attr.type + ".index"
+			return	'<input type="text" class="td-input-item transparent" ng-model="' + typeValue + '" value="{{' + typeValue + '}}" index={{' + typeIndex + '}} />';
+		},
+		link: function(scope,element,attrs){
+				element.on('change',function(event){
+					 PieChart.segments[$filter('searchChartId')(PieChart.segments, +attrs.index)].label = attrs.value;
+					 PieChart.update();
+				});
+				element.on('blur',function(){
+					element.addClass('transparent');
+				});
+				element.on('focus',function(){
+					element.removeClass("transparent");
+				});
+		}
+	};
+});
+///////////////////////////////////filter by id
+app.filter('searchChartId',function(){
+	return function(arr,id){
+			var i = 0, len = arr.length;
+			for(;i < len; i++){
+					if(i == id){
+					return i;
+				}
+			}
+
+	}
+
+});
